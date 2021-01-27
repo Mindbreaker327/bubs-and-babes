80a2dc80d3d358f5cae45828140ad47fb61237de
Mindbreaker327 <77804947+Mindbreaker327@users.noreply.github.com>
2021-01-27 07:26:50 -0500

    Fixed Shield powerup

--- main.ts
+++ main.ts
@@ -33,28 +33,40 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, ot
     PowerupType = randint(1, 2)
     if (PowerupType == 1) {
         PowerUp1 = sprites.create(img`
-            . . . . . . . . . c c 8 . . . . 
-            . . . . . . 8 c c c f 8 c c . . 
-            . . . c c 8 8 f c a f f f c c . 
-            . . c c c f f f c a a f f c c c 
-            8 c c c f f f f c c a a c 8 c c 
-            c c c b f f f 8 a c c a a a c c 
-            c a a b b 8 a b c c c c c c c c 
-            a f c a a b b a c c c c c f f c 
-            a 8 f c a a c c a c a c f f f c 
-            c a 8 a a c c c c a a f f f 8 a 
-            . a c a a c f f a a b 8 f f c a 
-            . . c c b a f f f a b b c c 6 c 
-            . . . c b b a f f 6 6 a b 6 c . 
-            . . . c c b b b 6 6 a c c c c . 
-            . . . . c c a b b c c c . . . . 
-            . . . . . c c c c c c . . . . . 
+            2222222222222222222222
+            2222222222222222222222
+            2222222222222222222222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            222................222
+            2222222222222222222222
+            2222222222222222222222
+            2222222222222222222222
             `, SpriteKind.Assistant)
-        PowerUp1.setPosition(MySpriteX, 100)
-        controller.moveSprite(PowerUp1, 100, 0)
+        PowerUp1.setPosition(MySpriteX, MySpriteY)
+        controller.moveSprite(PowerUp1, 100, 100)
     } else if (PowerupType == 2) {
         info.changeLifeBy(3)
     }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Assistant3, function (sprite, otherSprite) {
+    Obstacle.destroy()
+    Poweruptwo_3.destroy()
 })
 info.onCountdownEnd(function () {
     Powerup = sprites.create(img`
@@ -108,12 +120,9 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Assistant, function (sprite, othe
     Poweruptwo_2.setPosition(MySpriteX, 100)
     controller.moveSprite(Poweruptwo_2, 100, 0)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
-    Obstacle.destroy()
-    Poweruptwo_3.destroy()
-})
 let X = 0
 let Powerup: Sprite = null
+let MySpriteY = 0
 let PowerUp1: Sprite = null
 let PowerupType = 0
 let Poweruptwo_3: Sprite = null
@@ -244,4 +253,5 @@ game.onUpdateInterval(Time, function () {
 })
 game.onUpdateInterval(100, function () {
     MySpriteX = mySprite.x
+    MySpriteY = mySprite.y
 })
