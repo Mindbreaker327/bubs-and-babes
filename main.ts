fc10e4bb59653fdf5953969861e97aed4a7eec29
LukeAntone <43500295+LukeAntone@users.noreply.github.com>
2021-01-27 17:57:58 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -38,16 +38,16 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Assistant2, function (sprite, oth
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
         . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
+        . . . . f f 5 5 5 5 f f . . . . 
+        . . . f 5 5 5 f f 5 5 5 f . . . 
         . . f f f f f 2 2 f f f f f . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e 2 f 2 f f 2 f 2 e f . . 
-        . . f f f 2 2 e e 2 2 f f f . . 
-        . f f e f 2 f e e f 2 f e f f . 
-        . f e e f f e e e e f e e e f . 
-        . . f e e e e e e e e e e f . . 
-        . . . f e e e e e e e e f . . . 
+        . . f f 5 2 5 2 2 5 2 5 f f . . 
+        . . f 5 2 f 2 f f 2 f 2 5 f . . 
+        . . f f f 2 2 5 5 2 2 f f f . . 
+        . . f 5 f 2 f 5 5 f 2 f 5 f . . 
+        . . f 5 f f 5 5 5 5 f 5 5 f . . 
+        . . f 5 5 5 5 5 5 5 5 5 5 f . . 
+        . . . f 5 5 5 5 5 5 5 5 f . . . 
         . . e 4 f f f f f f f f 4 e . . 
         . . 4 d f 2 2 2 2 2 2 f d 4 . . 
         . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
@@ -141,16 +141,16 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Assistant3, function (sprite, ot
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
         . . . . f f f f f f . . . . . . 
-        . . . f 2 f e e e e f f . . . . 
-        . . f 2 2 2 f e e e e f f . . . 
-        . . f e e e e f f e e e f . . . 
-        . f e 2 2 2 2 e e f f f f . . . 
-        . f 2 e f f f f 2 2 2 e f . . . 
-        . f f f e e e f f f f f f f . . 
-        . f e e 4 4 f b e 4 4 e f f . . 
-        . . f e d d f 1 4 d 4 e e f . . 
-        . . . f d d d d 4 e e e f . . . 
-        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 2 f 5 5 5 5 f f . . . . 
+        . . f 2 2 2 f 5 5 5 5 f f . . . 
+        . . f 5 5 5 5 f f 5 5 5 f . . . 
+        . f 5 2 2 2 2 5 5 f f f f . . . 
+        . f 2 5 f f f f 2 2 2 5 f . . . 
+        . f f f 5 5 5 f f f f f f f . . 
+        . f 5 5 4 4 f b 5 4 4 5 f f . . 
+        . . f 5 d d f 1 4 d 4 5 5 f . . 
+        . . . f d d d d 4 5 5 5 f . . . 
+        . . . f 5 4 4 4 5 5 f f . . . . 
         . . . f 2 2 2 e d d 4 . . . . . 
         . . . f 2 2 2 e d d e . . . . . 
         . . . f 5 5 4 f e e f . . . . . 
@@ -163,22 +163,22 @@ scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.floorLight0, function (spr
 })
 info.onCountdownEnd(function () {
     Powerup = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . b 5 5 b . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . b b b b b 5 5 5 5 5 5 5 b . . 
-        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-        . . b d 5 5 b 1 f f 5 4 4 c . . 
-        b b d b 5 5 5 d f b 4 4 4 4 b . 
-        b d d c d 5 5 b 5 4 4 4 4 4 4 b 
-        c d d d c c b 5 5 5 5 5 5 5 b . 
-        c b d d d d d 5 5 5 5 5 5 5 b . 
-        . c d d d d d d 5 5 5 5 5 d b . 
-        . . c b d d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
+        . 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . 
+        4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        4 5 c 5 5 5 5 5 5 5 5 5 5 c 5 f 
+        4 5 5 5 5 1 1 1 1 1 5 5 5 5 5 f 
+        4 5 5 5 1 1 f f f 1 1 5 5 5 5 f 
+        4 5 5 5 1 1 f 5 5 1 1 f 5 5 5 f 
+        4 5 5 5 1 1 f 5 5 1 1 f 5 5 5 f 
+        4 5 5 5 5 f f 5 1 1 1 f 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 f f f 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 f 5 5 5 5 5 f 
+        4 5 5 5 5 5 5 5 f f 5 5 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 5 5 5 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 f 5 5 5 5 5 f 
+        4 5 c 5 5 5 5 5 f f 5 5 5 c 5 f 
+        4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f f f f f f f f f f f f f f . 
         `, SpriteKind.Boost)
     Powerup.setVelocity(0, 50)
     Powerup.setPosition(randint(160, 0), 0)
@@ -224,16 +224,16 @@ sprites.onOverlap(SpriteKind.Boost, SpriteKind.Player, function (sprite, otherSp
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
         . . . . . . f f f f f f . . . . 
-        . . . . f f e e e e f 2 f . . . 
-        . . . f f e e e e f 2 2 2 f . . 
-        . . . f e e e f f e e e e f . . 
-        . . . f f f f e e 2 2 2 2 e f . 
-        . . . f e 2 2 2 f f f f e 2 f . 
-        . . f f f f f f f e e e f f f . 
-        . . f f e 4 4 e b f 4 4 e e f . 
-        . . f e e 4 d 4 1 f d d e f . . 
-        . . . f e e e 4 d d d d f . . . 
-        . . . . f f e e 4 4 4 e f . . . 
+        . . . . f f 5 5 5 5 f 2 f . . . 
+        . . . f f 5 5 5 5 f 2 2 2 f . . 
+        . . . f 5 5 5 f f 5 5 5 5 f . . 
+        . . . f f f f 5 5 2 2 2 2 5 f . 
+        . . . f 5 2 2 2 f f f f 5 2 f . 
+        . . . f f f f f f 5 5 5 f f f . 
+        . . . f 5 4 4 e b f 4 4 5 5 f . 
+        . . . f 5 4 d 4 1 f d d 5 f . . 
+        . . . f 5 5 5 4 d d d d f . . . 
+        . . . . f f 5 5 4 4 4 e f . . . 
         . . . . . 4 d d e 2 2 2 f . . . 
         . . . . . e d d e 2 2 2 f . . . 
         . . . . . f e e f 4 5 5 f . . . 
@@ -279,16 +279,16 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Assistant, function (sprite, othe
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
         . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
+        . . . . f f f 5 5 f f f . . . . 
+        . . . f f f 5 5 5 5 f f f . . . 
+        . . f f f 5 5 5 5 5 5 f f f . . 
+        . . f f 5 5 5 5 5 5 5 5 5 f . . 
+        . . f 5 5 f f f f f f 5 5 f . . 
+        . . f f f f 5 5 5 5 f f f f . . 
+        . f f 5 f b f 4 4 f b f 5 f f . 
+        . f 5 5 4 1 f d d f 1 4 5 5 f . 
+        . . f 5 5 d d d d d d 5 5 f . . 
+        . . . f 5 5 4 4 4 4 5 5 f . . . 
         . . e 4 f 2 2 2 2 2 2 f 4 e . . 
         . . 4 d f 2 2 2 2 2 2 f d 4 . . 
         . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
@@ -315,16 +315,16 @@ let MySpriteX = 0
 MySpriteX = 80
 mySpriteUp = sprites.create(img`
     . . . . . . f f f f . . . . . . 
-    . . . . f f e e e e f f . . . . 
-    . . . f e e e f f e e e f . . . 
+    . . . . f f 5 5 5 5 f f . . . . 
+    . . . f 5 5 5 f f 5 5 5 f . . . 
     . . f f f f f 2 2 f f f f f . . 
-    . . f f e 2 e 2 2 e 2 e f f . . 
-    . . f e 2 f 2 f f 2 f 2 e f . . 
-    . . f f f 2 2 e e 2 2 f f f . . 
-    . f f e f 2 f e e f 2 f e f f . 
-    . f e e f f e e e e f e e e f . 
-    . . f e e e e e e e e e e f . . 
-    . . . f e e e e e e e e f . . . 
+    . . f f 5 2 5 2 2 5 2 5 f f . . 
+    . . f 5 2 f 2 f f 2 f 2 5 f . . 
+    . . f f f 2 2 5 5 2 2 f f f . . 
+    . f f 5 f 2 f 5 5 f 2 f 5 f f . 
+    . f 5 5 f f 5 5 5 5 f 5 5 5 f . 
+    . . f 5 5 5 5 5 5 5 5 5 5 f . . 
+    . . . f 5 5 5 5 5 5 5 5 f . . . 
     . . e 4 f f f f f f f f 4 e . . 
     . . 4 d f 2 2 2 2 2 2 f d 4 . . 
     . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
