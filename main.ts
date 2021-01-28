01ede02a2c7ed43ed546ded633237db00af11b07
Mindbreaker327 <77804947+Mindbreaker327@users.noreply.github.com>
2021-01-28 14:00:52 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -9,71 +9,51 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Assistant2, function (sprite, oth
     Obstacle.destroy()
     Poweruptwo_2.destroy()
     Poweruptwo_3 = sprites.create(img`
-        ....................
-        ....888888888888....
-        ...88..........88...
-        ..88............88..
-        .88..............88.
-        88................88
-        8..................8
-        8..................8
-        8..................8
-        8..................8
-        8..................8
-        8..................8
-        8..................8
-        8..................8
-        88................88
-        .88..............88.
-        ..88............88..
-        ...88..........88...
-        ....888888888888....
-        ....................
-        ....................
-        ....................
+        22222222222222222222
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        2..................2
+        22222222222222222222
         `, SpriteKind.Assistant3)
     Poweruptwo_3.setPosition(MySpriteX, MySpriteY)
     controller.moveSprite(Poweruptwo_3, 100, 100)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        ..........................
-        ..........................
-        ..........................
-        11111111111111111111111111
-        12221222122221222122212221
-        12121211111211211121111211
-        12221222111211222121111211
-        12211211121211211121111211
-        12121222122211222122211211
-        11111111111111111111111111
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
         `)
-})
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        ..........................
-        ..........................
-        ..........................
-        11111111111111111111111111
-        12221222122221222122212221
-        12121211111211211121111211
-        12221222111211222121111211
-        12211211121211211121111211
-        12121222122211222122211211
-        11111111111111111111111111
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        `, mySpriteUp, 0, 50)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
@@ -93,26 +73,26 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         ..........................
         ..........................
         ..........................
-        `, mySpriteUp, 0, -50)
+        `, mySpriteUp, 0, 50)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        ..........................
-        ..........................
-        ..........................
-        11111111111111111111111111
-        12221222122221222122212221
-        12121211111211211121111211
-        12221222111211222121111211
-        12211211121211211121111211
-        12121222122211222122211211
-        11111111111111111111111111
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 2 2 2 e d d 4 . . . . . 
+        . . . f 2 2 2 e d d e . . . . . 
+        . . . f 5 5 4 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
         `)
 })
 scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.floorLight0, function (sprite, location) {
@@ -120,22 +100,22 @@ scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.floorLight0, function (spr
 })
 info.onCountdownEnd(function () {
     Powerup = sprites.create(img`
-        ..........................
-        ..........................
-        ..........................
-        11111111111111111111111111
-        12221222122221222122212221
-        12121211111211211121111211
-        12221222111211222121111211
-        12211211121211211121111211
-        12121222122211222122211211
-        11111111111111111111111111
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 4 b 
+        b d d c d 5 5 b 5 4 4 4 4 4 b . 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
         `, SpriteKind.Boost)
     Powerup.setVelocity(0, 50)
     Powerup.setPosition(randint(160, 0), 0)
@@ -184,22 +164,22 @@ sprites.onOverlap(SpriteKind.Boost, SpriteKind.Player, function (sprite, otherSp
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        ..........................
-        ..........................
-        ..........................
-        11111111111111111111111111
-        12221222122221222122212221
-        12121211111211211121111211
-        12221222111211222121111211
-        12211211121211211121111211
-        12121222122211222122211211
-        11111111111111111111111111
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
         `)
 })
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
@@ -239,22 +219,22 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Assistant, function (sprite, othe
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        ..........................
-        ..........................
-        ..........................
-        11111111111111111111111111
-        12221222122221222122212221
-        12121211111211211121111211
-        12221222111211222121111211
-        12211211121211211121111211
-        12121222122211222122211211
-        11111111111111111111111111
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
-        ..........................
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
         `)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
@@ -276,19 +256,19 @@ let MySpriteX = 0
 MySpriteX = 80
 mySpriteUp = sprites.create(img`
     . . . . . . f f f f . . . . . . 
-    . . . . f f e e e e f f . . . . 
-    . . . f e e e f f e e e f . . . 
-    . . f f f f f 2 2 f f f f f . . 
-    . . f f e 2 e 2 2 e 2 e f f . . 
-    . . f e 2 f 2 f f 2 f 2 e f . . 
-    . . f f f 2 2 e e 2 2 f f f . . 
-    . f f e f 2 f e e f 2 f e f f . 
-    . f e e f f e e e e f e e e f . 
-    . . f e e e e e e e e e e f . . 
-    . . . f e e e e e e e e f . . . 
-    . . e 4 f f f f f f f f 4 e . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
     . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
@@ -353,22 +333,22 @@ game.onUpdateInterval(Time, function () {
         Obstacle.setVelocity(0, VY * -1)
     } else if (X == 3) {
         Obstacle = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
             . . . f 4 4 f f f f . . . . . . 
             . . f 4 5 5 4 5 f b f f . . . . 
             . . f 5 5 5 5 4 e 3 3 b f . . . 
             . . f e 4 4 4 e 3 3 3 3 b f . . 
-            . f 3 3 3 3 3 3 3 3 3 3 3 f . . 
+            . . f 3 3 3 3 3 3 3 3 3 3 f . . 
             . f 3 3 e e 3 b e 3 3 3 3 f . . 
             . f 3 3 e e e f f 3 3 3 3 f . . 
-            . . f e e e f b f b b b b f f . 
-            . . . e 4 4 f 1 e b b b b b f . 
-            . . . f 4 4 4 4 f b b b b b f . 
-            . . . f d d d e 4 4 b b b f . . 
-            . . . f d d d e 4 4 f f f . . . 
-            . . f d d d b b e e b b f . . . 
-            . . f b d 1 d 1 d d b f . . . . 
-            . . . f f f b b f f f . . . . . 
+            . f 3 e e e f b f b b b b f . . 
+            . . f e 4 4 f 1 e b b b b f . . 
+            . . . f 4 4 4 4 f b b b b f f . 
+            . . . f e e e f f f b b b b f . 
+            . . . f d d d e 4 4 f b b f . . 
+            . . . f d d d e 4 4 e f f . . . 
+            . . f b d b d b e e b f . . . . 
+            . . f f 1 d 1 d 1 d f f . . . . 
+            . . . . f f b b f f . . . . . . 
             `, SpriteKind.Enemy)
         Obstacle.setPosition(130, randint(140, 20))
         Obstacle.setVelocity(VX * -1, 0)
