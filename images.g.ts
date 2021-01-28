f093794108b0589ebcf9141e00feaf8424ba0e95
Mindbreaker327 <77804947+Mindbreaker327@users.noreply.github.com>
2021-01-28 14:13:35 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -879,30 +879,6 @@ f b b b b b b b b b b b b b b f
 2222222222222222222222
 2222222222222222222222
 `;
-            case "image19":return img`
-22222222222222222222
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-2..................2
-22222222222222222222
-`;
             case "image37":return img`
 . . . . . . f f f f 4 4 f . . . 
 . . . . f f b f 5 4 5 5 4 f . . 
@@ -1071,7 +1047,69 @@ f b b b b b b b b b b b b b b f
 . . . . f f d 1 d 1 d 1 f f . . 
 . . . . . . f f b b f f . . . . 
 `;
-            case "image55":return img`
+            case "image57":return img`
+. . . . . f f 4 4 f f . . . . . 
+. . . . f 5 4 5 5 4 5 f . . . . 
+. . . f e 3 3 3 3 3 3 e f . . . 
+. . f b 3 3 3 3 3 3 3 3 b f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+. f b 3 3 3 3 3 3 3 3 3 3 b f . 
+. f b b 3 3 3 3 3 3 3 3 b b f . 
+. f b b b b b b b b b b b b f . 
+f c b b b b b b b b b b b b c f 
+f b b b b b b b b b b b b b b f 
+. f c c b b b b b b b b c c f . 
+. . e 4 c f f f f f f c 4 e . . 
+. . e f b d b d b d b b f e . . 
+. . . f f 1 d 1 d 1 d f f . . . 
+. . . . . f f b b f f . . . . . 
+`;
+            case "image58":return img`
+. . . . . . . . . . . . . . . . 
+. . . f 4 4 f f f f . . . . . . 
+. . f 4 5 5 4 5 f b f f . . . . 
+. . f 5 5 5 5 4 e 3 3 b f . . . 
+. . f e 4 4 4 e 3 3 3 3 b f . . 
+. f 3 3 3 3 3 3 3 3 3 3 3 f . . 
+. f 3 3 e e 3 b e 3 3 3 3 f . . 
+. f 3 3 e e e f f 3 3 3 3 f . . 
+. . f e e e f b f b b b b f f . 
+. . . e 4 4 f 1 e b b b b b f . 
+. . . f 4 4 4 4 f b b b b b f . 
+. . . f d d d e 4 4 b b b f . . 
+. . . f d d d e 4 4 f f f . . . 
+. . f d d d b b e e b b f . . . 
+. . f b d 1 d 1 d d b f . . . . 
+. . . f f f b b f f f . . . . . 
+`;
+            case "image59":return img`
+2222222222222222222222
+2222222222222222222222
+2222222222222222222222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+222................222
+2222222222222222222222
+2222222222222222222222
+2222222222222222222222
+`;
+            case "image61":return img`
 . . . . . . f f f f 4 4 f . . . 
 . . . . f f b f 5 4 5 5 4 f . . 
 . . . f b 3 3 e 4 5 5 5 5 f . . 
@@ -1089,7 +1127,43 @@ f b b b b b b b b b b b b b b f
 . . . . f f d 1 d 1 d 1 f f . . 
 . . . . . . f f b b f f . . . . 
 `;
-            case "image56":return img`
+            case "image62":return img`
+. . . . . . f f f f 4 4 f . . . 
+. . . . f f b f 5 4 5 5 4 f . . 
+. . . f b 3 3 e 4 5 5 5 5 f . . 
+. . f b 3 3 3 3 e 4 4 4 e f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. . f 3 3 3 3 e b 3 e e 3 3 f . 
+. . f 3 3 3 3 f f e e e 3 3 f . 
+. . f b b b b f b f e e e 3 f . 
+. . f b b b b e 1 f 4 4 e f . . 
+. f f b b b b f 4 4 4 4 f . . . 
+. f b b b b f f f e e e f . . . 
+. . f b b f 4 4 e d d d f . . . 
+. . . f f e 4 4 e d d d f . . . 
+. . . . f b e e b d b d b f . . 
+. . . . f f d 1 d 1 d 1 f f . . 
+. . . . . . f f b b f f . . . . 
+`;
+            case "image63":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image64":return img`
 22222222222222222222
 2..................2
 2..................2
@@ -1112,6 +1186,96 @@ f b b b b b b b b b b b b b b f
 2..................2
 2..................2
 22222222222222222222
+`;
+            case "image65":return img`
+. . . . . f f 4 4 f f . . . . . 
+. . . . f 5 4 5 5 4 5 f . . . . 
+. . . f e 4 5 5 5 5 4 e f . . . 
+. . f b 3 e 4 4 4 4 e 3 b f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. f 3 3 e b 3 e e 3 b e 3 3 f . 
+. f 3 3 f f e e e e f f 3 3 f . 
+. f b b f b f e e f b f b b f . 
+. f b b e 1 f 4 4 f 1 e b b f . 
+f f b b f 4 4 4 4 4 4 f b b f f 
+f b b f f f e e e e f f f b b f 
+. f e e f b d d d d b f e e f . 
+. . e 4 c d d d d d d c 4 e . . 
+. . e f b d b d b d b b f e . . 
+. . . f f 1 d 1 d 1 d f f . . . 
+. . . . . f f b b f f . . . . . 
+`;
+            case "image66":return img`
+. . . . . f f 4 4 f f . . . . . 
+. . . . f 5 4 5 5 4 5 f . . . . 
+. . . f e 3 3 3 3 3 3 e f . . . 
+. . f b 3 3 3 3 3 3 3 3 b f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+. f b 3 3 3 3 3 3 3 3 3 3 b f . 
+. f b b 3 3 3 3 3 3 3 3 b b f . 
+. f b b b b b b b b b b b b f . 
+f c b b b b b b b b b b b b c f 
+f b b b b b b b b b b b b b b f 
+. f c c b b b b b b b b c c f . 
+. . e 4 c f f f f f f c 4 e . . 
+. . e f b d b d b d b b f e . . 
+. . . f f 1 d 1 d 1 d f f . . . 
+. . . . . f f b b f f . . . . . 
+`;
+            case "image67":return img`
+. . . f 4 4 f f f f . . . . . . 
+. . f 4 5 5 4 5 f b f f . . . . 
+. . f 5 5 5 5 4 e 3 3 b f . . . 
+. . f e 4 4 4 e 3 3 3 3 b f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. f 3 3 e e 3 b e 3 3 3 3 f . . 
+. f 3 3 e e e f f 3 3 3 3 f . . 
+. f 3 e e e f b f b b b b f . . 
+. . f e 4 4 f 1 e b b b b f . . 
+. . . f 4 4 4 4 f b b b b f f . 
+. . . f e e e f f f b b b b f . 
+. . . f d d d e 4 4 f b b f . . 
+. . . f d d d e 4 4 e f f . . . 
+. . f b d b d b e e b f . . . . 
+. . f f 1 d 1 d 1 d f f . . . . 
+. . . . f f b b f f . . . . . . 
+`;
+            case "image68":return img`
+. . . . . . f f f f 4 4 f . . . 
+. . . . f f b f 5 4 5 5 4 f . . 
+. . . f b 3 3 e 4 5 5 5 5 f . . 
+. . f b 3 3 3 3 e 4 4 4 e f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. . f 3 3 3 3 e b 3 e e 3 3 f . 
+. . f 3 3 3 3 f f e e e 3 3 f . 
+. . f b b b b f b f e e e 3 f . 
+. . f b b b b e 1 f 4 4 e f . . 
+. f f b b b b f 4 4 4 4 f . . . 
+. f b b b b f f f e e e f . . . 
+. . f b b f 4 4 e d d d f . . . 
+. . . f f e 4 4 e d d d f . . . 
+. . . . f b e e b d b d b f . . 
+. . . . f f d 1 d 1 d 1 f f . . 
+. . . . . . f f b b f f . . . . 
+`;
+            case "image69":return img`
+. . . . . . f f f f 4 4 f . . . 
+. . . . f f b f 5 4 5 5 4 f . . 
+. . . f b 3 3 e 4 5 5 5 5 f . . 
+. . f b 3 3 3 3 e 4 4 4 e f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. . f 3 3 3 3 e b 3 e e 3 3 f . 
+. . f 3 3 3 3 f f e e e 3 3 f . 
+. . f b b b b f b f e e e 3 f . 
+. . f b b b b e 1 f 4 4 e f . . 
+. f f b b b b f 4 4 4 4 f . . . 
+. f b b b b f f f e e e f . . . 
+. . f b b f 4 4 e d d d f . . . 
+. . . f f e 4 4 e d d d f . . . 
+. . . . f b e e b d b d b f . . 
+. . . . f f d 1 d 1 d 1 f f . . 
+. . . . . . f f b b f f . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -9,50 +9,44 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Assistant2, function (sprite, oth
     Obstacle.destroy()
     Poweruptwo_2.destroy()
     Poweruptwo_3 = sprites.create(img`
-        22222222222222222222
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        2..................2
-        22222222222222222222
+        ...88888888888888888......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...8...............8......
+        ...88888888888888888......
         `, SpriteKind.Assistant3)
     Poweruptwo_3.setPosition(MySpriteX, MySpriteY)
     controller.moveSprite(Poweruptwo_3, 100, 100)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
-        . . f f f f f 2 2 f f f f f . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e 2 f 2 f f 2 f 2 e f . . 
-        . . f f f 2 2 e e 2 2 f f f . . 
-        . f f e f 2 f e e f 2 f e f f . 
-        . f e e f f e e e e f e e e f . 
-        . . f e e e e e e e e e e f . . 
-        . . . f e e e e e e e e f . . . 
-        . . e 4 f f f f f f f f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
+        ..........................
+        ..........................
+        ..........................
+        11111111111111111111111111
+        12221222122221222122212221
+        12121211111211211121111211
+        12221222111211222121111211
+        12211211121211211121111211
+        12121222122211222122211211
+        11111111111111111111111111
+        ..........................
+        ..........................
+        ..........................
+        ..........................
+        ..........................
+        ..........................
         `)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -77,22 +71,22 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        . . . . f f f f f f . . . . . . 
-        . . . f 2 f e e e e f f . . . . 
-        . . f 2 2 2 f e e e e f f . . . 
-        . . f e e e e f f e e e f . . . 
-        . f e 2 2 2 2 e e f f f f . . . 
-        . f 2 e f f f f 2 2 2 e f . . . 
-        . f f f e e e f f f f f f f . . 
-        . f e e 4 4 f b e 4 4 e f f . . 
-        . . f e d d f 1 4 d 4 e e f . . 
-        . . . f d d d d 4 e e e f . . . 
-        . . . f e 4 4 4 e e f f . . . . 
-        . . . f 2 2 2 e d d 4 . . . . . 
-        . . . f 2 2 2 e d d e . . . . . 
-        . . . f 5 5 4 f e e f . . . . . 
-        . . . . f f f f f f . . . . . . 
-        . . . . . . f f f . . . . . . . 
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 3 3 3 3 3 3 e f . . . 
+        . . f b 3 3 3 3 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . f b 3 3 3 3 3 3 3 3 3 3 b f . 
+        . f b b 3 3 3 3 3 3 3 3 b b f . 
+        . f b b b b b b b b b b b b f . 
+        f c b b b b b b b b b b b b c f 
+        f b b b b b b b b b b b b b b f 
+        . f c c b b b b b b b b c c f . 
+        . . e 4 c f f f f f f c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
         `)
 })
 scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.floorLight0, function (sprite, location) {
@@ -100,22 +94,22 @@ scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.floorLight0, function (spr
 })
 info.onCountdownEnd(function () {
     Powerup = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . b b b b b 5 5 5 5 5 5 5 b . . 
-        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-        . . b d 5 5 b 1 f f 5 4 4 c . . 
-        b b d b 5 5 5 d f b 4 4 4 4 4 b 
-        b d d c d 5 5 b 5 4 4 4 4 4 b . 
-        c d d d c c b 5 5 5 5 5 5 5 b . 
-        c b d d d d d 5 5 5 5 5 5 5 b . 
-        . c d d d d d d 5 5 5 5 5 d b . 
-        . . c b d d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
+        . 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . 
+        4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        4 5 c 5 5 5 5 5 5 5 5 5 5 c 5 f 
+        4 5 5 5 5 1 1 1 1 1 5 5 5 5 5 f 
+        4 5 5 5 1 1 f f f 1 1 5 5 5 5 f 
+        4 5 5 5 1 1 f 5 5 1 1 f 5 5 5 f 
+        4 5 5 5 1 1 f 5 5 1 1 f 5 5 5 f 
+        4 5 5 5 5 f f 5 1 1 1 f 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 f f f 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 f 5 5 5 5 5 f 
+        4 5 5 5 5 5 5 5 f f 5 5 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 5 5 5 5 5 5 f 
+        4 5 5 5 5 5 5 1 1 f 5 5 5 5 5 f 
+        4 5 c 5 5 5 5 5 f f 5 5 5 c 5 f 
+        4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+        . f f f f f f f f f f f f f f . 
         `, SpriteKind.Boost)
     Powerup.setVelocity(0, 50)
     Powerup.setPosition(randint(160, 0), 0)
@@ -131,30 +125,22 @@ sprites.onOverlap(SpriteKind.Boost, SpriteKind.Player, function (sprite, otherSp
     PowerupType = randint(1, 2)
     if (PowerupType == 1) {
         PowerUp1 = sprites.create(img`
-            2222222222222222222222
-            2222222222222222222222
-            2222222222222222222222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            222................222
-            2222222222222222222222
-            2222222222222222222222
-            2222222222222222222222
+            ....888888888888888888....
+            ....888888888888888888....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....88..............88....
+            ....888888888888888888....
+            ....888888888888888888....
             `, SpriteKind.Assistant)
         PowerUp1.setPosition(MySpriteX, MySpriteY)
         controller.moveSprite(PowerUp1, 100, 100)
@@ -164,22 +150,22 @@ sprites.onOverlap(SpriteKind.Boost, SpriteKind.Player, function (sprite, otherSp
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        . . . . . . f f f f f f . . . . 
-        . . . . f f e e e e f 2 f . . . 
-        . . . f f e e e e f 2 2 2 f . . 
-        . . . f e e e f f e e e e f . . 
-        . . . f f f f e e 2 2 2 2 e f . 
-        . . . f e 2 2 2 f f f f e 2 f . 
-        . . f f f f f f f e e e f f f . 
-        . . f f e 4 4 e b f 4 4 e e f . 
-        . . f e e 4 d 4 1 f d d e f . . 
-        . . . f e e e 4 d d d d f . . . 
-        . . . . f f e e 4 4 4 e f . . . 
-        . . . . . 4 d d e 2 2 2 f . . . 
-        . . . . . e d d e 2 2 2 f . . . 
-        . . . . . f e e f 4 5 5 f . . . 
-        . . . . . . f f f f f f . . . . 
-        . . . . . . . f f f . . . . . . 
+        ..........................
+        ..........................
+        ..........................
+        11111111111111111111111111
+        12221222122221222122212221
+        12121211111211211121111211
+        12221222111211222121111211
+        12211211121211211121111211
+        12121222122211222122211211
+        11111111111111111111111111
+        ..........................
+        ..........................
+        ..........................
+        ..........................
+        ..........................
+        ..........................
         `)
 })
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
@@ -190,51 +176,52 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Assistant, function (sprite, othe
     Obstacle.destroy()
     PowerUp1.destroy()
     Poweruptwo_2 = sprites.create(img`
-        222222222222222222222
-        222222222222222222222
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        22.................22
-        222222222222222222222
-        222222222222222222222
+        2222222222222222222222
+        2222222222222222222222
+        2222222222222222222222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        222................222
+        2222222222222222222222
+        2222222222222222222222
+        2222222222222222222222
         `, SpriteKind.Assistant2)
     Poweruptwo_2.setPosition(MySpriteX, MySpriteY)
     controller.moveSprite(Poweruptwo_2, 100, 100)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySpriteUp.setImage(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . f 4 4 f f f f . . . . . . 
+        . . f 4 5 5 4 5 f b f f . . . . 
+        . . f 5 5 5 5 4 e 3 3 b f . . . 
+        . . f e 4 4 4 e 3 3 3 3 b f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e e 3 b e 3 3 3 3 f . . 
+        . f 3 3 e e e f f 3 3 3 3 f . . 
+        . . f e e e f b f b b b b f f . 
+        . . . e 4 4 f 1 e b b b b b f . 
+        . . . f 4 4 4 4 f b b b b b f . 
+        . . . f d d d e 4 4 b b b f . . 
+        . . . f d d d e 4 4 f f f . . . 
+        . . f d d d b b e e b b f . . . 
+        . . f b d 1 d 1 d d b f . . . . 
+        . . . f f f b b f f f . . . . . 
         `)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
@@ -293,27 +280,6 @@ game.onUpdateInterval(Time, function () {
         Obstacle = sprites.create(img`
             . . . . . f f 4 4 f f . . . . . 
             . . . . f 5 4 5 5 4 5 f . . . . 
-            . . . f e 4 5 5 5 5 4 e f . . . 
-            . . f b 3 e 4 4 4 4 e 3 b f . . 
-            . . f 3 3 3 3 3 3 3 3 3 3 f . . 
-            . f 3 3 e b 3 e e 3 b e 3 3 f . 
-            . f 3 3 f f e e e e f f 3 3 f . 
-            . f b b f b f e e f b f b b f . 
-            . f b b e 1 f 4 4 f 1 e b b f . 
-            f f b b f 4 4 4 4 4 4 f b b f f 
-            f b b f f f e e e e f f f b b f 
-            . f e e f b d d d d b f e e f . 
-            . . e 4 c d d d d d d c 4 e . . 
-            . . e f b d b d b d b b f e . . 
-            . . . f f 1 d 1 d 1 d f f . . . 
-            . . . . . f f b b f f . . . . . 
-            `, SpriteKind.Enemy)
-        Obstacle.setPosition(randint(140, 20), 30)
-        Obstacle.setVelocity(0, VY)
-    } else if (X == 2) {
-        Obstacle = sprites.create(img`
-            . . . . . f f 4 4 f f . . . . . 
-            . . . . f 5 4 5 5 4 5 f . . . . 
             . . . f e 3 3 3 3 3 3 e f . . . 
             . . f b 3 3 3 3 3 3 3 3 b f . . 
             . . f 3 3 3 3 3 3 3 3 3 3 f . . 
@@ -329,30 +295,30 @@ game.onUpdateInterval(Time, function () {
             . . . f f 1 d 1 d 1 d f f . . . 
             . . . . . f f b b f f . . . . . 
             `, SpriteKind.Enemy)
-        Obstacle.setPosition(randint(140, 20), 130)
-        Obstacle.setVelocity(0, VY * -1)
-    } else if (X == 3) {
+        Obstacle.setPosition(randint(140, 20), 30)
+        Obstacle.setVelocity(0, VY)
+    } else if (X == 2) {
         Obstacle = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
             . . . f 4 4 f f f f . . . . . . 
             . . f 4 5 5 4 5 f b f f . . . . 
             . . f 5 5 5 5 4 e 3 3 b f . . . 
             . . f e 4 4 4 e 3 3 3 3 b f . . 
-            . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+            . f 3 3 3 3 3 3 3 3 3 3 3 f . . 
             . f 3 3 e e 3 b e 3 3 3 3 f . . 
             . f 3 3 e e e f f 3 3 3 3 f . . 
-            . f 3 e e e f b f b b b b f . . 
-            . . f e 4 4 f 1 e b b b b f . . 
-            . . . f 4 4 4 4 f b b b b f f . 
-            . . . f e e e f f f b b b b f . 
-            . . . f d d d e 4 4 f b b f . . 
-            . . . f d d d e 4 4 e f f . . . 
-            . . f b d b d b e e b f . . . . 
-            . . f f 1 d 1 d 1 d f f . . . . 
-            . . . . f f b b f f . . . . . . 
+            . . f e e e f b f b b b b f f . 
+            . . . e 4 4 f 1 e b b b b b f . 
+            . . . f 4 4 4 4 f b b b b b f . 
+            . . . f d d d e 4 4 b b b f . . 
+            . . . f d d d e 4 4 f f f . . . 
+            . . f d d d b b e e b b f . . . 
+            . . f b d 1 d 1 d d b f . . . . 
+            . . . f f f b b f f f . . . . . 
             `, SpriteKind.Enemy)
-        Obstacle.setPosition(130, randint(140, 20))
-        Obstacle.setVelocity(VX * -1, 0)
-    } else {
+        Obstacle.setPosition(randint(140, 20), 130)
+        Obstacle.setVelocity(0, VY * -1)
+    } else if (X == 3) {
         Obstacle = sprites.create(img`
             . . . . . . f f f f 4 4 f . . . 
             . . . . f f b f 5 4 5 5 4 f . . 
@@ -370,6 +336,27 @@ game.onUpdateInterval(Time, function () {
             . . . . f b e e b d b d b f . . 
             . . . . f f d 1 d 1 d 1 f f . . 
             . . . . . . f f b b f f . . . . 
+            `, SpriteKind.Enemy)
+        Obstacle.setPosition(130, randint(140, 20))
+        Obstacle.setVelocity(VX * -1, 0)
+    } else {
+        Obstacle = sprites.create(img`
+            . . . . . f f 4 4 f f . . . . . 
+            . . . . f 5 4 5 5 4 5 f . . . . 
+            . . . f e 4 5 5 5 5 4 e f . . . 
+            . . f b 3 e 4 4 4 4 e 3 b f . . 
+            . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+            . f 3 3 e b 3 e e 3 b e 3 3 f . 
+            . f 3 3 f f e e e e f f 3 3 f . 
+            . f b b f b f e e f b f b b f . 
+            . f b b e 1 f 4 4 f 1 e b b f . 
+            f f b b f 4 4 4 4 4 4 f b b f f 
+            f b b f f f e e e e f f f b b f 
+            . f e e f b d d d d b f e e f . 
+            . . e 4 c d d d d d d c 4 e . . 
+            . . e f b d b d b d b b f e . . 
+            . . . f f 1 d 1 d 1 d f f . . . 
+            . . . . . f f b b f f . . . . . 
             `, SpriteKind.Enemy)
         Obstacle.setPosition(30, randint(140, 20))
         Obstacle.setVelocity(VX, 0)
