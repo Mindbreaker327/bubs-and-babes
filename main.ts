287b27b90b9bcb7e1490f6b5fef058fc25559b39
LukeAntone <43500295+LukeAntone@users.noreply.github.com>
2021-02-03 21:28:53 -0500

    Update pxt.json, images.g.jres, images.g.ts, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -452,14 +452,14 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
         . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
+        . . f f f 5 5 5 5 5 5 f f f . . 
+        . . f f 5 2 2 2 2 2 2 5 5 f . . 
+        . . f 5 2 f f f f f f 2 5 f . . 
+        . . f f f f 5 5 5 5 f f f f . . 
+        . f f f f b f 4 4 f b f f f f . 
+        . f f f 4 1 f d d f 1 4 f f f . 
+        . . f f d d d d d d d d f f . . 
+        . . . f d d 4 4 4 4 d d f . . . 
         . . e 4 f 2 2 2 2 2 2 f 4 e . . 
         . . 4 d f 2 2 2 2 2 2 f d 4 . . 
         . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
@@ -472,14 +472,14 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
         . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
+        . . f f f 5 5 5 5 5 5 f f f . . 
+        . . f f 5 2 2 2 2 2 2 5 5 f . . 
+        . . f 5 2 f f f f f f 2 5 f . . 
+        . . f f f f 5 5 5 5 f f f f . . 
+        . f f f f b f 4 4 f b f f f f . 
+        . f f f 4 1 f d d f 1 4 f f f . 
+        . . f f d d d d d d d d f f . . 
+        . . . f d d 4 4 4 4 d d f . . . 
         . . e 4 f 2 2 2 2 2 2 f 4 e . . 
         . . 4 d f 2 2 2 2 2 2 f d 4 . . 
         . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
@@ -490,12 +490,12 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
         . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . f f e 2 f f f f f f 2 e f f . 
-        . f f f f f e e e e f f f f f . 
-        . . f e f b f 4 4 f b f e f . . 
-        . . f e 4 1 f d d f 1 4 e f . . 
+        . . f f f 5 5 5 5 5 5 f f f . . 
+        . . f f 5 2 2 2 2 2 2 5 5 f . . 
+        . f f 5 2 f f f f f f 2 5 f f . 
+        . f f f f f 5 5 5 5 f f f f f . 
+        . . f 5 f b f 4 4 f b f 5 f . . 
+        . . f 5 4 1 f d d f 1 4 5 f . . 
         . . . f e 4 d d d d 4 e f e . . 
         . . f e f 2 2 2 2 e d d 4 e . . 
         . . e 4 f 2 2 2 2 e d d e . . . 
@@ -506,14 +506,14 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
         . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
+        . . f f f 5 5 5 5 5 5 f f f . . 
+        . . f f 5 2 2 2 2 2 2 5 5 f . . 
+        . . f 5 2 f f f f f f 2 5 f . . 
+        . . f f f f 5 5 5 5 f f f f . . 
+        . f f f f b f 4 4 f b f f f f . 
+        . f f f 4 1 f d d f 1 4 f f f . 
+        . . f f d d d d d d d d f f . . 
+        . . . f d d 4 4 4 4 d d f . . . 
         . . e 4 f 2 2 2 2 2 2 f 4 e . . 
         . . 4 d f 2 2 2 2 2 2 f d 4 . . 
         . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
@@ -524,12 +524,12 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
         . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f e e 2 2 2 2 2 2 e f f . . 
-        . f f e 2 f f f f f f 2 e f f . 
-        . f f f f f e e e e f f f f f . 
-        . . f e f b f 4 4 f b f e f . . 
-        . . f e 4 1 f d d f 1 4 e f . . 
+        . . f f f 5 5 5 5 5 5 f f f . . 
+        . . f 5 5 2 2 2 2 2 2 5 f f . . 
+        . f f 5 2 f f f f f f 2 5 f f . 
+        . f f f f f 5 5 5 5 f f f f f . 
+        . . f 5 f b f 4 4 f b f 5 f . . 
+        . . f 5 4 1 f d d f 1 4 5 f . . 
         . . e f e 4 d d d d 4 e f . . . 
         . . e 4 d d e 2 2 2 2 f e f . . 
         . . . e d d e 2 2 2 2 f 4 e . . 
