dfd87ec3357d0fd90fd140e72de60e69c48c710a
LukeAntone <43500295+LukeAntone@users.noreply.github.com>
2021-02-11 14:00:39 -0500

    Update images.g.jres, images.g.ts, main.ts

--- images.g.ts
+++ images.g.ts
@@ -295,24 +295,6 @@ e 4 f b b b b b b f 4 e
 . . . f f f f f f . . . 
 . . . f f . . f f . . . 
 `;
-            case "image60":return img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
-. . f f f f e e e e f f f f . . 
-. f f e f b f 4 4 f b f e f f . 
-. f e e 4 1 f d d f 1 4 e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e 4 4 4 4 e e f . . . 
-. . e 4 f 2 2 2 2 2 2 f 4 e . . 
-. . 4 d f 2 2 2 2 2 2 f d 4 . . 
-. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`;
             case "image1":return img`
 1 1 1 1 1 1 1 1 1 1 1 1 1 
 1 2 1 2 1 2 2 2 1 2 2 2 1 
@@ -420,6 +402,24 @@ f b b 4 1 f d d f 1 4 b b f
 . 4 f b 3 b 3 b 3 b b f 4 . 
 . . f f 3 b 3 b 3 3 f f . . 
 . . . . f f b b f f . . . . 
+`;
+            case "image60":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f 5 2 2 2 2 5 f f . . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. . f 5 5 2 2 2 2 2 2 5 5 f . . 
+. . f 5 2 f f f f f f 2 5 f . . 
+. . f f 5 f 5 5 5 5 f 5 f f . . 
+. f f 5 f b f 4 4 f b f 5 f f . 
+. f 5 5 4 1 f d d f 1 4 5 5 f . 
+. . f f f d d d d d d f f f . . 
+. . . f f f 4 4 4 4 f f f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
 `;
         }
         return null;
