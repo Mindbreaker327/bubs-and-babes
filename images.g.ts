f60223d7fa80e2c6bdf3f256c8cc4755b4a3bff3
LukeAntone <43500295+LukeAntone@users.noreply.github.com>
2021-02-17 14:11:48 -0500

    Update pxt.json, images.g.jres, images.g.ts, main.blocks, main.ts

--- images.g.ts
+++ images.g.ts
@@ -3558,75 +3558,6 @@ f a a a d 1 f 4 4 f 1 d a a a f
 . . . . f f f 8 8 8 f f f f . . 
 . . . . . f f . . . f f f . . . 
 `];
-            case "anim85":return [img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f 5 5 5 5 5 5 f f f . . 
-. . f f 5 2 2 2 2 2 2 5 5 f . . 
-. . f 5 2 f f f f f f 2 5 f . . 
-. . f f f f 5 5 5 5 f f f f . . 
-. f f f f b f 4 4 f b f f f f . 
-. f f f 4 1 f d d f 1 4 f f f . 
-. . f f d d d d d d d d f f . . 
-. . . f d d 4 4 4 4 d d f . . . 
-. . e 4 f 2 2 2 2 2 2 f 4 e . . 
-. . 4 d f 2 2 2 2 2 2 f d 4 . . 
-. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f 5 5 5 5 5 5 f f f . . 
-. . f f 5 2 2 2 2 2 2 5 5 f . . 
-. f f 5 2 f f f f f f 2 5 f f . 
-. f f f f f 5 5 5 5 f f f f f . 
-. . f 5 f b f 4 4 f b f 5 f . . 
-. . f 5 4 1 f d d f 1 4 5 f . . 
-. . . f e 4 d d d d 4 e f e . . 
-. . f e f 2 2 2 2 e d d 4 e . . 
-. . e 4 f 2 2 2 2 e d d e . . . 
-. . . . f 4 4 5 5 f e e . . . . 
-. . . . f f f f f f f . . . . . 
-. . . . f f f . . . . . . . . . 
-`, img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f 5 5 5 5 5 5 f f f . . 
-. . f f 5 2 2 2 2 2 2 5 5 f . . 
-. . f 5 2 f f f f f f 2 5 f . . 
-. . f f f f 5 5 5 5 f f f f . . 
-. f f f f b f 4 4 f b f f f f . 
-. f f f 4 1 f d d f 1 4 f f f . 
-. . f f d d d d d d d d f f . . 
-. . . f d d 4 4 4 4 d d f . . . 
-. . e 4 f 2 2 2 2 2 2 f 4 e . . 
-. . 4 d f 2 2 2 2 2 2 f d 4 . . 
-. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f 5 5 5 5 5 5 f f f . . 
-. . f 5 5 2 2 2 2 2 2 5 f f . . 
-. f f 5 2 f f f f f f 2 5 f f . 
-. f f f f f 5 5 5 5 f f f f f . 
-. . f 5 f b f 4 4 f b f 5 f . . 
-. . f 5 4 1 f d d f 1 4 5 f . . 
-. . e f e 4 d d d d 4 e f . . . 
-. . e 4 d d e 2 2 2 2 f e f . . 
-. . . e d d e 2 2 2 2 f 4 e . . 
-. . . . e e f 5 5 4 4 f . . . . 
-. . . . . f f f f f f f . . . . 
-. . . . . . . . . f f f . . . . 
-`];
             case "anim86":return [img`
 . . . . . f f 8 8 f f . . . . . 
 . . . . f 8 8 8 8 8 8 f . . . . 
@@ -3885,6 +3816,75 @@ f a a a d 1 f 4 4 f 1 d a a a f
 . . d f b b d b d d d d f . . . 
 . . . f f 1 1 d 1 d 1 f f . . . 
 . . . . . f b b f f f . . . . . 
+`];
+            case "anim85":return [img`
+. . . . . . f f f f . . . . . . 
+. . . . f f 8 8 8 8 f f . . . . 
+. . . f f 8 8 8 8 8 8 f f . . . 
+. . f f 8 8 8 8 8 8 8 8 f f . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. f f 5 f b f 4 4 f b f 5 f f . 
+. f 5 5 4 1 f d d f 1 4 5 5 f . 
+. . f f d d d d d d d d f f . . 
+. . . f d d 4 4 4 4 d d f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d d d d d d d d d d 4 . . 
+. . 4 4 d 8 8 8 8 8 8 d 4 4 . . 
+. . . . . d 8 8 8 8 d . . . . . 
+. . . . . d d . . d d . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f 8 8 8 8 f f . . . . 
+. . . f f 8 8 8 8 8 8 f f . . . 
+. . f f 8 8 8 8 8 8 8 8 f f . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. f f 8 8 8 8 8 8 8 8 8 8 f f . 
+. f f f 5 5 5 5 5 5 5 5 f f f . 
+. . f 5 f b f 4 4 f b f 5 f . . 
+. . f 5 4 1 f d d f 1 4 5 f . . 
+. . . f e 4 d d d d 4 e f e . . 
+. . f e f 2 2 2 2 e d d 4 e . . 
+. . e 4 d d d d d e d d e . . . 
+. . . . d 8 8 8 8 8 e e . . . . 
+. . . . d d 8 8 8 d d . . . . . 
+. . . . d d d . . . . . . . . . 
+`, img`
+. . . . . . f f f f . . . . . . 
+. . . . f f 8 8 8 8 f f . . . . 
+. . . f f 8 8 8 8 8 8 f f . . . 
+. . f f 8 8 8 8 8 8 8 8 f f . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. f f 5 f b f 4 4 f b f 5 f f . 
+. f 5 5 4 1 f d d f 1 4 5 5 f . 
+. . f f d d d d d d d d f f . . 
+. . . f d d 4 4 4 4 d d f . . . 
+. . e 4 2 2 2 2 2 2 2 2 4 e . . 
+. . 4 d d d d d d d d d d 4 . . 
+. . 4 4 d 8 8 8 8 8 8 d 4 4 . . 
+. . . . . d 8 8 8 8 d . . . . . 
+. . . . . d d . . d d . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f 8 8 8 8 f f . . . . 
+. . . f f 8 8 8 8 8 8 f f . . . 
+. . f f 8 8 8 8 8 8 8 8 f f . . 
+. . f 8 8 8 8 8 8 8 8 8 8 f . . 
+. f f 8 8 8 8 8 8 8 8 8 8 f f . 
+. f f f 5 5 5 5 5 5 5 5 f f f . 
+. . f 5 f b f 4 4 f b f 5 f . . 
+. . f 5 4 1 f d d f 1 4 5 f . . 
+. . e f e 4 d d d d 4 e f . . . 
+. . e 4 d d e 2 2 2 2 f e f . . 
+. . . e d d e d d d d d 4 e . . 
+. . . . e e 8 8 8 8 8 8 . . . . 
+. . . . . d d 8 8 8 8 d . . . . 
+. . . . . . . . . d d d . . . . 
 `];
         }
         return null;
